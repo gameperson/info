8494377a0390741dfb5a0cbca2bf928a96163712
gameperson <gamesomeotherday@gmail.com>
2025-07-13 18:35:18 +0000

    save old theme-switch-js

---
+++ res/js/theme-switch-old.js
@@ -0,0 +1,12 @@
+// theme-switch.js
+function toggleTheme() {
+    document.body.classList.toggle('light-theme');  // Toggle the light theme class
+
+    // Get all theme icons
+    const icons = document.querySelectorAll('img[aria-label="Toggle Theme"]');
+    
+    // Set the icon source to the light mode icon
+    icons.forEach(icon => {
+        icon.src = '../images/bulb_white.png'; // Change to the desired icon
+    });
+}
